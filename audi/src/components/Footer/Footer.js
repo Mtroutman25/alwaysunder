de707b52aea4f253a3303d576a048e69c74f9aa5
Mike Troutman <mktrout09@gmail.com>
2018-03-26 21:32:14 -0400

    added js for components

---
+++ audi/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () =>
+  <footer className="footer">
+    <span>Basket Builder @2018 A.U.D.I.</span>
+  </footer>;
+
+export default Footer;
